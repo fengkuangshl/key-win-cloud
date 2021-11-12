ddebe5b61cdd57faccaece3008e88bd0816d1cf6
fengkuangshl <fengkuangshl@163.com>
2021-11-12 10:51:41 +0800

    优化

--- web-portal/back-center-vue/src/router/index.ts
+++ web-portal/back-center-vue/src/router/index.ts
@@ -12,25 +12,26 @@ export const constantRoutes: Array<RouteConfig> = [
     path: '/login',
     name: 'Login',
     component: () => import(/* webpackChunkName: "login" */ '@/views/login/Login.vue')
-  },
-  {
-    path: '/index',
-    name: 'Index',
-    redirect: '/home',
-    component: () => import(/* webpackChunkName: "index" */ '@/views/index/Index.vue'),
-    children: [
-      {
-        path: '/home',
-        name: 'home',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/index/home/Home.vue')
-      },
-      {
-        path: '/test',
-        name: 'Test',
-        component: () => import(/* webpackChunkName: "test" */ '@/views/index/test/Test.vue')
-      }
-    ]
   }
+  // ,
+  // {
+  //   path: '/index',
+  //   name: 'Index',
+  //   redirect: '/home',
+  //   component: () => import(/* webpackChunkName: "index" */ '@/views/index/Index.vue'),
+  //   children: [
+  //     {
+  //       path: '/home',
+  //       name: 'home',
+  //       component: () => import(/* webpackChunkName: "home" */ '@/views/index/home/Home.vue')
+  //     },
+  //     {
+  //       path: '/test',
+  //       name: 'Test',
+  //       component: () => import(/* webpackChunkName: "test" */ '@/views/index/test/Test.vue')
+  //     }
+  //   ]
+  // }
   // ,
   // {
   //   path: '*',
