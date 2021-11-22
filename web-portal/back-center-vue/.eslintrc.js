746ef2a2cd5371e41b77be5c75f6f279e4dd899d
fengkuangshl <fengkuangshl@163.com>
2021-11-22 11:18:20 +0800

    webpack

--- web-portal/back-center-vue/.eslintrc.js
+++ web-portal/back-center-vue/.eslintrc.js
@@ -11,7 +11,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': 0
+    // 左括号前没有空格报错
+    'space-before-function-paren': 0,
+    // 在格式化的时候不加分号
+    semi: false,
+    // 将双引号转成单引号
+    singleQuote: true
   },
   globals: {
     KWResponse: true,
---
+++ web-portal/back-center-vue/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: {
+    // 配置端口
+    port: 8888,
+    // 配置自动打开浏览器
+    open: true
+  }
+}
