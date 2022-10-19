b26aaeeb177fc799d32325973ddeb573ff03e426
fengkuangshl <fengkuangshl@163.com>
2022-10-19 17:19:22 +0800

    多数据源

--- new-api-gateway/src/main/java/com/key/win/ApiGateWayApp.java
+++ new-api-gateway/src/main/java/com/key/win/ApiGateWayApp.java
@@ -4,6 +4,7 @@ package com.key.win;
 import com.key.win.common.config.TraceFilterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.FilterType;
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = TraceFilterConfig.class))
-@SpringBootApplication 
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiGateWayApp {
     public static void main(String[] args) {
         SpringApplication.run(ApiGateWayApp.class, args);
